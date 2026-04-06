69baacd93019f1d4aa76da267da35ae7984780a3
npdmechtech-ai <npdmechtech@gmail.com>
2026-04-06 12:06:29 +0530

    Delete AiManual.API directory

--- AiManual.API/Models/ChatResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AiManual.API.Models
-{
-    public class ChatResponse
-    {
-        public string Heading { get; set; } = "";
-        public List<StepResponse> Steps { get; set; } = new List<StepResponse>();
-    }
-}
